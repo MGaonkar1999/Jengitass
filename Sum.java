e591c0174abf2e8a5d1e4e83d0e7c76f15170cde
MGaonkar1999 <mvgaonkar1999@gmail.com>
2023-04-29 21:25:05 +0530

    New created revised file

--- Sum.java
+++ Sum.java
@@ -1,23 +1,12 @@
-import java.util.*;
 public class Sum
 {
 public static void main(String[] args) 
 {
-Scanner s=new Scanner(System.in);
 int a,b,c;
-char ch;
-do
-{
-System.out.println("Enter first num");
-a=s.nextInt();
-System.out.println("Enter first num");
-b=s.nextInt();
+a=100;
+b=150;
 c=a+b;
 System.out.println("Sum of 2 numbers" + c);			
-System.out.print("Do you want to continue y/n? ");
-ch = s.next().charAt(0);			
-System.out.println();
-}
-while(ch=='y' || ch=='Y');             
 }
 }
+
